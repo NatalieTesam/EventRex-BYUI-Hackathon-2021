67ce3d232923a248f6caebb4d7fd6985f89f7cfc
Levi Jones <leviwill.j@gmail.com>
2021-10-06 12:59:18 -0600

    Created JS to fill the event queues with events

--- js/events.js
+++ js/events.js
@@ -1,9 +1,46 @@
-const events = (function () {
-    function createEvent(name) {
-        console.log('creating event with name ' + name);
+const Events = (function () {
+    const testEvents = [
+        { name: 'test name', imgSrc: 'images/randomimage.jpg', desc: 'lorem ipsum dolor sit amet' },
+        { name: 'event name', imgSrc: 'images/randomimage.jpg', desc: 'a great event!' },
+        { name: 'party!!', imgSrc: 'images/randomimage.jpg', desc: 'it will knock your socks off!' },
+        { name: 'some friends gaming', imgSrc: 'images/mariokart.png', desc: 'anyone up for some mariokart?' },
+        { name: 'soda mixer', imgSrc: 'images/randomimage.jpg', desc: 'Come and mix some new Sodas, meet new people, and just have fun!' }
+    ];
+
+    function PopulateContainer(containerID) {
+        let containerEle = document.getElementById(containerID);
+
+        for (let i = 0; i < testEvents.length; i++) {
+            containerEle.appendChild(CreateEventCard(testEvents[i].name, testEvents[i].imgSrc, testEvents[i].desc));
+        }
+    }
+
+    function CreateEventCard(name, imgSrc, desc) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+
+        let img = document.createElement('img');
+        img.src = imgSrc;
+
+        let container = document.createElement('div');
+        container.classList.add('container');
+
+        let title = document.createElement('h3');
+        title.innerText = name;
+
+        let descParagraph = document.createElement('p');
+        descParagraph.innerText = desc;
+
+        container.appendChild(title);
+        container.appendChild(descParagraph);
+
+        card.appendChild(img);
+        card.appendChild(container);
+
+        return card;
     }
 
     return {
-        createEvent: createEvent
+        PopulateContainer: PopulateContainer
     }
 })();
