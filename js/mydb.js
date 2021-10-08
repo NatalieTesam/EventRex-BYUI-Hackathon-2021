3198075e7b4f00b898b6a1e1ba8f15c6e540655b
Levi Jones <leviwill.j@gmail.com>
2021-10-08 13:27:33 -0600

    database test

--- js/mydb.js
+++ js/mydb.js
@@ -1,7 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
-
-const app = initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyCPmLNhrIRjznM5QPHPimj-smI9FyTTssQ",
     authDomain: "eventrex-328317.firebaseapp.com",
     projectId: "eventrex-328317",
@@ -10,8 +7,3 @@ const app = initializeApp({
     appId: "1:23091519004:web:349f167605c8d3c29c49d9",
     measurementId: "G-45W9RE82W6"
 });
-const analytics = getAnalytics(app);
-
-window.getFirebaseApp = function() {
-    return app;
-}
