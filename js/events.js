3a11547d6b8cdea81bade13c7055e89852d41b35
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-10-09 18:20:42 -0600

    fr

--- js/events.js
+++ js/events.js
@@ -125,7 +125,7 @@ const events = (function () {
                 resultCards.push(CreateEventCard(docData.name, docData.desc, docData.category, 'username', doc.id));
                 console.log(i, querySnapshot.size);
                 if (i == querySnapshot.size) {
-                    console.log(result);
+                    console.log(resultCard);
                     result.appendChild(resultCards);
                 }
                 i++;
